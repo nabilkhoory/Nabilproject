a32e98b66de1ee2306ed7b4243631aec7d070265
root <root@ip-172-31-30-73.us-west-2.compute.internal>
2017-10-24 05:28:36 +0000

    test

---
+++ tests/EmailTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+	
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers Email
+  */
+  final class EmailTest extends TestCase
+  {
+      public function testCanBeCreatedFromValidEmailAddress(): void
+          {
+	          $this->assertInstanceOf(
+		              Email::class,
+			                  Email::fromString('nabilkhoury2001@yahoo.com')
+					          );
+						      }
+
+    public function testCannotBeCreatedFromInvalidEmailAddress(): void
+        {
+	        $this->expectException(InvalidArgumentException::class);
+
+        Email::fromString('invalid');
+	    }
+
+    public function testCanBeUsedAsString(): void
+        {
+	        $this->assertEquals(
+		            'nabilkhoury2001@yahoo.com',
+			                Email::fromString('nabilkhoury2001@yahoo.com')
+					        );
+						    }
+						    }
+						    
